e277157c62cd2810a41b3308c266702b4bf8cb86
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-23 00:55:34 +0000

    Deploy educkf/static-nuxt-store to github.com/educkf/static-nuxt-store.git:gh-pages

---
+++ _nuxt/static/1595465715/posts/UanmNg9VDxdv0yqYwaAq/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1595465715",layout:"default",error:a,state:{_BaseStore:{list:[]},BlogStore:{list:[]},Notification:{icon:a,type:a,title:a,message:a,button:a,buttonAction:a,timeout:a},User:{}},serverRendered:true,routePath:"\u002Fposts\u002FUanmNg9VDxdv0yqYwaAq",config:{}}}(null));
